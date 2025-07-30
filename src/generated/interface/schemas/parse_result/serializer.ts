f72a95be9a941709f5ae5802949dc89fb42e7191
Corno Schraverus <corno@schraverus.com>
2025-07-30 13:49:33 +0200

    latest generated code

---
+++ src/generated/interface/schemas/parse_result/serializer.ts
@@ -0,0 +1,187 @@
+import * as _pt from 'exupery-core-types'
+
+import * as _i_in from "./unconstrained"
+import * as _i_out from "../../core/astn_target"
+
+// **** TYPES
+
+export type _T_Annotated_Token = (
+    $$_: _i_in._T_Annotated_Token,
+    $$_p: null,
+) => _i_out._T_Value
+
+export type _T_Parse_Error = (
+    $$_: _i_in._T_Parse_Error,
+    $$_p: null,
+) => _i_out._T_Value
+
+export type _T_Parse_Result = (
+    $$_: _i_in._T_Parse_Result,
+    $$_p: null,
+) => _i_out._T_Value
+
+export type _T_Token_Type = (
+    $$_: _i_in._T_Token_Type,
+    $$_p: null,
+) => _i_out._T_Value
+
+export type _T_Tokenizer_Result = (
+    $$_: _i_in._T_Tokenizer_Result,
+    $$_p: null,
+) => _i_out._T_Value
+
+// **** FRIENDLY NAMES FOR THE GLOBAL TYPES
+
+export type Annotated_Token = _T_Annotated_Token
+
+export type Parse_Error = _T_Parse_Error
+
+export type Parse_Result = _T_Parse_Result
+
+export type Token_Type = _T_Token_Type
+
+export type Tokenizer_Result = _T_Tokenizer_Result
+
+// **** ALIASES FOR NESTED TYPE WITH PREFIXED ROOT NAMES
+
+export namespace _T_Annotated_Token {
+    
+    export namespace CONTEXT {
+    }
+    export type CONTEXT = _i_in._T_Annotated_Token
+    
+    export namespace PARAMS {
+    }
+    
+    export namespace RESULT {
+    }
+    export type RESULT = _i_out._T_Value
+}
+
+export namespace _T_Parse_Error {
+    
+    export namespace CONTEXT {
+    }
+    export type CONTEXT = _i_in._T_Parse_Error
+    
+    export namespace PARAMS {
+    }
+    
+    export namespace RESULT {
+    }
+    export type RESULT = _i_out._T_Value
+}
+
+export namespace _T_Parse_Result {
+    
+    export namespace CONTEXT {
+    }
+    export type CONTEXT = _i_in._T_Parse_Result
+    
+    export namespace PARAMS {
+    }
+    
+    export namespace RESULT {
+    }
+    export type RESULT = _i_out._T_Value
+}
+
+export namespace _T_Token_Type {
+    
+    export namespace CONTEXT {
+    }
+    export type CONTEXT = _i_in._T_Token_Type
+    
+    export namespace PARAMS {
+    }
+    
+    export namespace RESULT {
+    }
+    export type RESULT = _i_out._T_Value
+}
+
+export namespace _T_Tokenizer_Result {
+    
+    export namespace CONTEXT {
+    }
+    export type CONTEXT = _i_in._T_Tokenizer_Result
+    
+    export namespace PARAMS {
+    }
+    
+    export namespace RESULT {
+    }
+    export type RESULT = _i_out._T_Value
+}
+
+// *** ALIASES FOR NESTED TYPES
+
+export namespace Annotated_Token {
+    
+    export namespace CONTEXT {
+    }
+    export type CONTEXT = _i_in._T_Annotated_Token
+    
+    export namespace PARAMS {
+    }
+    
+    export namespace RESULT {
+    }
+    export type RESULT = _i_out._T_Value
+}
+
+export namespace Parse_Error {
+    
+    export namespace CONTEXT {
+    }
+    export type CONTEXT = _i_in._T_Parse_Error
+    
+    export namespace PARAMS {
+    }
+    
+    export namespace RESULT {
+    }
+    export type RESULT = _i_out._T_Value
+}
+
+export namespace Parse_Result {
+    
+    export namespace CONTEXT {
+    }
+    export type CONTEXT = _i_in._T_Parse_Result
+    
+    export namespace PARAMS {
+    }
+    
+    export namespace RESULT {
+    }
+    export type RESULT = _i_out._T_Value
+}
+
+export namespace Token_Type {
+    
+    export namespace CONTEXT {
+    }
+    export type CONTEXT = _i_in._T_Token_Type
+    
+    export namespace PARAMS {
+    }
+    
+    export namespace RESULT {
+    }
+    export type RESULT = _i_out._T_Value
+}
+
+export namespace Tokenizer_Result {
+    
+    export namespace CONTEXT {
+    }
+    export type CONTEXT = _i_in._T_Tokenizer_Result
+    
+    export namespace PARAMS {
+    }
+    
+    export namespace RESULT {
+    }
+    export type RESULT = _i_out._T_Value
+}
