363ac16f33aaf366078bb3a3381cfe8f43803e5b
Corno Schraverus <corno@schraverus.com>
2025-08-05 11:11:12 +0200

    removed index

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-export * as d_ast from "./generated/interface/schemas/ast/unconstrained"
-export * as d_ide from "./generated/interface/schemas/ide/unconstrained"
-export * as d_parse_result from "./generated/interface/schemas/parse_result/unconstrained"
-
-export * as t_parse from "./parse/parse"
-export * as t_format from "./transformations/ast/ide"
-export * as t_create_error_message from "./serializers/parse_result"
