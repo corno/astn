564ee6402a1032c5c5456e91c73818f1c82d60ed
Corno Schraverus <corno@schraverus.com>
2025-09-28 21:12:46 +0200

    .

--- src/bin/convert_to_json.ts
+++ src/bin/convert_to_json.ts
@@ -15,29 +15,33 @@ import { $$ as log_error } from "exupery-resources/dist/commands/log_error"
 import { $$ as get_instream_data } from "exupery-resources/dist/queries/get_instream_data"
 
 
-_eb.run_unsafe_program(() => get_instream_data(
-).execute_unsafe_command(($) =>
-    _ea.cc(parse.parse(
-        $,
-        {
-            'tab size': 4,
+_eb.run_unsafe_program(
+    ($) => get_instream_data(
+    ).execute_unsafe_command(($) => _ea.cc(
+        parse.parse(
+            $,
+            {
+                'tab size': 4,
+            }
+        ),
+        ($) => {
+            switch ($[0]) {
+                case 'failure': return _ea.ss($, ($) => log_error(
+                    _ea.array_literal([
+                        `Parse Error: ${create_error_message.Parse_Error($, { 'position info': ['one based', null] })}`
+                    ])
+                ).cast_to_unsafe())
+                case 'success': return _ea.ss($, ($) => log(
+                    _ea.array_literal([s_json.Document(
+                        t_ast_2_json.Document($),
+                        {
+                            'indentation': "    ",
+                            'newline': '\n'
+                        }
+                    )])
+                ).throw_exception(1))
+                default: return _ea.au($[0])
+            }
         }
-    ), ($): _easync.Unsafe_Command_Result<number> => {
-        switch ($[0]) {
-            case 'failure': return _ea.ss($, ($) => {
-                // _er.temp_resources.console.error()
-                return log_error(`Parse Error: ${create_error_message.Parse_Error($, { 'position info': ['one based', null] })}`).cast_to_unsafe()
-            })
-            case 'success': return _ea.ss($, ($) => {
-                return log(s_json.Document(
-                    t_ast_2_json.Document($),
-                    {
-                        'indentation': "    ",
-                        'newline': '\n'
-                    }
-                )).throw_exception(1)
-            })
-            default: return _ea.au($[0])
-        }
-    })
-))
+    ))
+)
--- src/bin/validate_astn.ts
+++ src/bin/validate_astn.ts
@@ -8,29 +8,30 @@ import * as parse from "../parse/parse"
 import * as create_error_message from "../serializers/parse_result"
 
 import { $$ as log } from "exupery-resources/dist/commands/log"
+import { $$ as log_error } from "exupery-resources/dist/commands/log_error"
+import { $$ as get_instream_data } from "exupery-resources/dist/queries/get_instream_data"
 
 
-_eb.run_program(($) => {
-    return log("bla bla").then(() => _ea.panic("not implemented yet"))
-})
-
-// _er.temp_resources.process["get instream data"](($) => {
-//     _ea.cc(parse.parse(
-//         $,
-//         {
-//             'tab size': 4,
-//         }
-//     ), ($) => {
-//         switch ($[0]) {
-//             case 'failure': return _ea.ss($, ($) => {
-//                 _er.temp_resources.console.error(`Parse Error: ${create_error_message.Parse_Error($, { 'position info': ['one based', null] })}`)
-//                 // process.exit(1)
-//             })
-//             case 'success': return _ea.ss($, ($) => {
-//                 _er.temp_resources.console.log(`document is valid ASTN`)
-//                 // process.exit(0)
-//             })
-//             default: return _ea.au($[0])
-//         }
-//     })
-// })
+_eb.run_unsafe_program(
+    ($) => get_instream_data(
+    ).execute_unsafe_command(($) => _ea.cc(
+        parse.parse(
+            $,
+            {
+                'tab size': 4,
+            }
+        ),
+        ($): _easync.Unsafe_Command_Result<number> => {
+            switch ($[0]) {
+                case 'failure': return _ea.ss($, ($) => {
+                    // _er.temp_resources.console.error()
+                    return log_error(_ea.array_literal([`Parse Error: ${create_error_message.Parse_Error($, { 'position info': ['one based', null] })}`])).cast_to_unsafe()
+                })
+                case 'success': return _ea.ss($, ($) => {
+                    return log(_ea.array_literal(["document is valid ASTN"])).throw_exception(1)
+                })
+                default: return _ea.au($[0])
+            }
+        }
+    ))
+)
