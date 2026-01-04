57e7bd88369f25425d4a02eae2d0433fb0c37273
Corno Schraverus <corno@schraverus.com>
2026-01-04 10:28:17 +0100

    ...

--- pub/src/implementation/manual/schemas/parse_result/serializers.ts
+++ pub/src/implementation/manual/schemas/parse_result/serializers.ts
@@ -4,65 +4,63 @@ import * as d_parse_result from "../../../../interface/generated/pareto/schemas/
 
 import { $$ as s_list_of_separated_texts } from "pareto-standard-operations/dist/implementation/temp_serializers/schemas/list_of_separated_texts"
 
-export const Parse_Error_Type = ($: d_parse_result.Parse_Error._type): string => {
-    return _pt.cc($, ($) => {
-        switch ($[0]) {
-            case 'lexer': return _pt.ss($, ($) => _pt.cc($, ($) => {
+export const Parse_Error_Type = ($: d_parse_result.Parse_Error._type): string => _pt.cc($, ($) => {
+    switch ($[0]) {
+        case 'lexer': return _pt.ss($, ($) => _pt.cc($, ($) => {
+            switch ($[0]) {
+                case 'unexpected control character': return `found unexpected control character`
+                case 'unexpected end of line in delimited string': return `found unexpected end of line in delimited string`
+                case 'missing character after escape': return `found missing character after escape`
+                case 'unexpected character': return `found unexpected character`
+                case 'unterminated string': return `found unterminated string`
+                case 'unterminated block comment': return `found unterminated block comment`
+                case 'unterminated unicode escape sequence': return `found unterminated unicode escape sequence`
+                case 'invalid unicode escape sequence': return `found invalid unicode escape sequence`
+                case 'unknown escape character': return `found unknown escape character`
+                case 'unexpected end of input': return `found unexpected end of input`
+                case 'dangling slash': return `found dangling slash`
+                default: return _pt.au($[0])
+            }
+        }))
+        case 'parser': return _pt.ss($, ($) => `expected ${s_list_of_separated_texts(
+            $.expected.map(($) => _pt.cc($, ($) => {
                 switch ($[0]) {
-                    case 'unexpected control character': return `found unexpected control character`
-                    case 'unexpected end of line in delimited string': return `found unexpected end of line in delimited string`
-                    case 'missing character after escape': return `found missing character after escape`
-                    case 'unexpected character': return `found unexpected character`
-                    case 'unterminated string': return `found unterminated string`
-                    case 'unterminated block comment': return `found unterminated block comment`
-                    case 'unterminated unicode escape sequence': return `found unterminated unicode escape sequence`
-                    case 'invalid unicode escape sequence': return `found invalid unicode escape sequence`
-                    case 'unknown escape character': return `found unknown escape character`
-                    case 'unexpected end of input': return `found unexpected end of input`
-                    case 'dangling slash': return `found dangling slash`
+                    case '!': return "'!'"
+                    case ')': return "')'"
+                    case ',': return "','"
+                    case ':': return "':'"
+                    case '>': return "'>'"
+                    case '@': return "'@'"
+                    case ']': return "']'"
+                    case 'a string': return "a string"
+                    case 'a value': return "a value"
+                    case '}': return "'}'"
+                    case '#': return "'#'"
                     default: return _pt.au($[0])
                 }
-            }))
-            case 'parser': return _pt.ss($, ($) => `expected ${s_list_of_separated_texts(
-                $.expected.map(($) => _pt.cc($, ($) => {
-                    switch ($[0]) {
-                        case '!': return "'!'"
-                        case ')': return "')'"
-                        case ',': return "','"
-                        case ':': return "':'"
-                        case '>': return "'>'"
-                        case '@': return "'@'"
-                        case ']': return "']'"
-                        case 'a string': return "a string"
-                        case 'a value': return "a value"
-                        case '}': return "'}'"
-                        case '#': return "'#'"
-                        default: return _pt.au($[0])
-                    }
-                })), 
-                { 'separator': " or " }
-            )}, found ${_pt.cc($.cause, ($) => {
-                switch ($[0]) {
-                    case 'unexpected token': return _pt.ss($, ($) => {
-                        return $.found[0]
-                    })
-                    case 'missing token': return _pt.ss($, ($) => {
-                        return `nothing`
-                    })
-                    default: return _pt.au($[0])
-                }
-            })}`)
-            default: return _pt.au($[0])
-        }
-    })
-}
+            })),
+            { 'separator': " or " }
+        )}, found ${_pt.cc($.cause, ($) => {
+            switch ($[0]) {
+                case 'unexpected token': return _pt.ss($, ($) => {
+                    return $.found[0]
+                })
+                case 'missing token': return _pt.ss($, ($) => {
+                    return `nothing`
+                })
+                default: return _pt.au($[0])
+            }
+        })}`)
+        default: return _pt.au($[0])
+    }
+})
 
 export const Parse_Error = (
     $: d_parse_result.Parse_Error,
     $p: {
         'position info':
-        |[ 'zero based', null]
-        |[ 'one based', null]
+        | ['zero based', null]
+        | ['one based', null]
     }
 ): string => {
     const extra: number = _pt.cc($p['position info'], ($) => {
--- pub/src/shorthands/parse_result.ts
+++ pub/src/shorthands/parse_result.ts
@@ -8,42 +8,37 @@ import * as d_target_token from "../interface/generated/pareto/schemas/token/dat
 export const parse_error = (
     type: d_target.Parse_Error._type,
     range: d_target_token.Range
-): d_target.Parse_Error => {
-    return {
-        'type': type,
-        'range': {
-            'start': range.start,
-            'end': range.end,
-        }
+): d_target.Parse_Error => ({
+    'type': type,
+    'range': {
+        'start': range.start,
+        'end': range.end,
     }
-}
+})
 
 export const unexpected_token = (
     found: d_target_token.Annotated_Token,
     expected: _pi.List<d_target.Parse_Error._type.SG.parser.expected.L>,
-): d_target.Parse_Error => {
-    return parse_error(
-        ['parser', {
-            'expected': expected,
-            'cause': ['unexpected token', {
-                'found': found.type,
-            }]
-        }],
-        {
-            'start': found.start,
-            'end': found.end
-        }
-    )
-}
+): d_target.Parse_Error => parse_error(
+    ['parser', {
+        'expected': expected,
+        'cause': ['unexpected token', {
+            'found': found.type,
+        }]
+    }],
+    {
+        'start': found.start,
+        'end': found.end
+    }
+)
+
 export const lexer_error = (
     type: d_target.Parse_Error._type.SG.lexer,
     range: d_target_token.Range
-): d_target.Parse_Error => {
-    return {
-        'type': ['lexer', type],
-        'range': {
-            'start': range.start,
-            'end': range.end,
-        }
+): d_target.Parse_Error => ({
+    'type': ['lexer', type],
+    'range': {
+        'start': range.start,
+        'end': range.end,
     }
-}
+})
