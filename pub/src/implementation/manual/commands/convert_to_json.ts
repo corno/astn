c04f38a91067fd2cf068fff3091b96f1a44a69f3
Corno Schraverus <corno@schraverus.com>
2026-01-06 17:55:29 +0100

    ...

--- pub/src/implementation/manual/commands/convert_to_json.ts
+++ pub/src/implementation/manual/commands/convert_to_json.ts
@@ -7,7 +7,7 @@ import * as signatures from "../../../interface/signatures"
 //data types
 
 //dependencies
-import { $$ as create_file_to_file_command } from "../../../modules/default_tool_signatures/implementation/manual/command_creators/create_file_to_file_command"
+import { $$ as create_file_to_file_command } from "../../../modules/common_tool_signatures/implementation/manual/command_creators/create_file_to_file_command"
 import { $$ as my_func } from "../text_to_text/convert_to_json"
 
 
