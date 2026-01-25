7c81646a7be0ea1fe54cfd642f7209a00b494de1
Corno Schraverus <corno@schraverus.com>
2026-01-25 09:15:25 +0100

    ...

--- pub/src/interface/signatures/transformers/parse_tree/token.ts
+++
@@ -1,6 +0,0 @@
-import * as _pi from 'pareto-core/dist/interface'
-
-import * as d_in from "astn-core/dist/interface/generated/liana/schemas/parse_tree/data"
-import * as d_out from "astn-core/dist/interface/generated/liana/schemas/token/data"
-
-export type Value = _pi.Transformer<d_in.Value, d_out.Range>
