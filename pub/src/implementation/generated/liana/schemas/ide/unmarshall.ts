d214f54a6c0694ea51024a1e39fb6bceb908ef1d
Corno Schraverus <corno@schraverus.com>
2026-01-26 20:22:57 +0100

    ...

---
+++ pub/src/implementation/generated/liana/schemas/ide/unmarshall.ts
@@ -0,0 +1,8 @@
+
+import * as _p from "pareto-core/dist/refiner"
+
+import * as t_signatures from "../../../../../interface/generated/liana/schemas/ide/unmarshall"
+export const Relative_Location: t_signatures.Relative_Location = ($,) => _p.unreachable_code_path()
+export const Relative_Range: t_signatures.Relative_Range = ($,) => _p.unreachable_code_path()
+export const Text_Edits: t_signatures.Text_Edits = ($,) => _p.unreachable_code_path()
+export const Key_Value_Pairs_To_Be_Sorted: t_signatures.Key_Value_Pairs_To_Be_Sorted = ($,) => _p.unreachable_code_path()
