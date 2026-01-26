12703395c329e84246a775c627c7c1567a286f2c
Corno Schraverus <corno@schraverus.com>
2026-01-26 11:27:27 +0100

    ...

---
+++ pub/src/implementation/generated/liana/schemas/ide/serialize.ts
@@ -0,0 +1,12 @@
+
+import * as _p from "pareto-core/dist/serializer"
+
+import * as t_signatures from "../../../../../interface/generated/liana/schemas/ide/serialize"
+
+import * as v_serialize from "astn-core/dist/implementation/manual/schemas/sealed_target/serializers"
+
+import * as v_marshall from "./marshall"
+export const Relative_Location: t_signatures.Relative_Location = ($,) => v_serialize.Document(v_marshall.Relative_Location($))
+export const Relative_Range: t_signatures.Relative_Range = ($,) => v_serialize.Document(v_marshall.Relative_Range($))
+export const Text_Edits: t_signatures.Text_Edits = ($,) => v_serialize.Document(v_marshall.Text_Edits($))
+export const Key_Value_Pairs_To_Be_Sorted: t_signatures.Key_Value_Pairs_To_Be_Sorted = ($,) => v_serialize.Document(v_marshall.Key_Value_Pairs_To_Be_Sorted($))
