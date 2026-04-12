00880ef50a266d34c3c0b3d4d53cdf8f3fe69801
Corno Schraverus <corno@schraverus.com>
2026-04-12 08:39:30 +0200

    pdt: published version 0.113.69

--- pub/src/implementation/manual/transformers/parse_tree/text.ts
+++ pub/src/implementation/manual/transformers/parse_tree/text.ts
@@ -5,7 +5,7 @@ import * as d_in from "astn-core/dist/interface/generated/liana/schemas/parse_tr
 import * as d_out from "pareto-fountain-pen/dist/interface/generated/liana/schemas/text/data"
 
 // export type Value = _pi.Transformer<d_in.Value, d_out.Text>
-export type Document = _pi.Transformer<d_in.Document, d_out.Text>
+export type Document = _pi.Transformer_With_Parameter<d_in.Document, d_out.Text, { 'indentation': string, 'newline': string }   >
 
 //dependencies
 import * as t_authoring_target_to_text from "../authoring_target/text"
@@ -13,14 +13,11 @@ import * as t_to_authoring_target from "./authoring_target"
 
 
 
-export const Document: Document = ($) => {
+export const Document: Document = ($, $p) => {
     return t_authoring_target_to_text.Document(
         t_to_authoring_target.Document(
             $,
         ),
-        {
-            'indentation': "",
-            'newline': "\n",
-        }
+        $p,
     )
 }
