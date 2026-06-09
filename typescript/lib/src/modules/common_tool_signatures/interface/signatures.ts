652eb1c6f8a8db2f6cca0cb2e4a64f5df1c0437d
Corno Schraverus <corno@schraverus.com>
2026-06-09 13:50:15 +0200

    pdt: ...

--- typescript/lib/src/modules/common_tool_signatures/interface/signatures.ts
+++ typescript/lib/src/modules/common_tool_signatures/interface/signatures.ts
@@ -1,6 +1,7 @@
 import * as _pi from 'pareto-core/dist/interface'
 
 import * as resources_pareto from "pareto-resources/dist/interface/resources"
+import * as resources_pareto_stream from "pareto-stream/dist/interface/resources"
 
 export namespace queries {
 
@@ -11,11 +12,11 @@ export namespace commands {
     export type stream_in_to_stream_out = _pi.Command_Procedure<
         resources_pareto.resources.commands.main,
         {
-            'log error': resources_pareto.stream.commands.log_error
-            'log': resources_pareto.stream.commands.log
+            'log error': resources_pareto_stream.commands.log_error
+            'log': resources_pareto_stream.commands.log
         },
         {
-            'get instream data': resources_pareto.stream.queries.get_instream_data,
+            'get instream data': resources_pareto_stream.queries.get_instream_data,
         },
         null
     >
@@ -23,7 +24,7 @@ export namespace commands {
     export type file_in_to_file_out = _pi.Command_Procedure<
         resources_pareto.resources.commands.main,
         {
-            'log error': resources_pareto.stream.commands.log_error
+            'log error': resources_pareto_stream.commands.log_error
             'write file': resources_pareto.filesystem_unrestricted.commands.write_file
         },
         {
