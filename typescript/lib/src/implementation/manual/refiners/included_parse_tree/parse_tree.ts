2744b258616a12b68a490bb81176ea828b276d66
Corno Schraverus <corno@schraverus.com>
2026-06-23 11:33:44 +0200

    pdt: ...

--- typescript/lib/src/implementation/manual/refiners/included_parse_tree/parse_tree.ts
+++ typescript/lib/src/implementation/manual/refiners/included_parse_tree/parse_tree.ts
@@ -11,7 +11,9 @@
 //     'included files': p_di.Dictionary<d_out.Value>
 // }
 
-// export const Document: p_i.Refiner_With_Parameter<d_in.Document, Error, d_out.Document, Parameters> = ($, abort, $p) => ({
+// export const Document: p_i.Refiner_With_Parameter<
+// d_in.Document, Error, d_out.Document, Parameters
+// > = ($, abort, $p) => ({
 //     'header': p_.from.optional($.header).map(
 //($) => ({
 //         '!': $['!'],
@@ -20,7 +22,9 @@
 //     'content': Value($.content, abort, $p),
 // })
 
-// export const Value: p_i.Refiner_With_Parameter<d_in.Value, Error, d_out.Value, Parameters> = ($, abort, $p) => p_.decide.state($.type, ($): d_out.Value => {
+// export const Value: p_i.Refiner_With_Parameter<
+// d_in.Value, Error, d_out.Value, Parameters
+// > = ($, abort, $p) => p_.decide.state($.type, ($): d_out.Value => {
 //     switch ($[0]) {
 //         case 'concrete': return p_.ss($, ($) => p_.from.state($).decide(
 //($):d_out.Value => {
@@ -64,10 +68,14 @@
 //     }
 // })
 
-// export const Items: p_i.Refiner_With_Parameter<d_in.Items, Error, d_out.Items, Parameters> = ($) => p_.from.list($).flatten(
+// export const Items: p_i.Refiner_With_Parameter<
+// d_in.Items, Error, d_out.Items, Parameters
+// > = ($) => p_.from.list($).flatten(
 // ($) => Value($.value))
 
-// export const ID_Value_Pairs: p_i.Refiner_With_Parameter<d_in.ID_Value_Pairs, Error, d_out.ID_Value_Pairs, Parameters> = ($) => p_.from.list($).flatten(
+// export const ID_Value_Pairs: p_i.Refiner_With_Parameter<
+// d_in.ID_Value_Pairs, Error, d_out.ID_Value_Pairs, Parameters
+// > = ($) => p_.from.list($).flatten(
 // ($ => $.assignment.__ decide(
 //     ($) => $.value.__ decide(
 //         ($) => Value($),
