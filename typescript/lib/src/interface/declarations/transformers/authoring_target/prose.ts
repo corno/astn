0e21bfa460de289550f543783d52c4a8ea4fefec
Corno Schraverus <corno@schraverus.com>
2026-07-08 12:36:59 +0200

    pdt: ...

--- typescript/lib/src/interface/declarations/transformers/authoring_target/prose.ts
+++ typescript/lib/src/interface/declarations/transformers/authoring_target/prose.ts
@@ -1,1 +1,30 @@
-import type * as p_ from 'pareto-core/interface/transformer'
+import * as p_ from 'pareto-core/implementation/transformer'
+import type * as p_i from 'pareto-core/interface/transformer'
+
+//data types
+import type * as d_out from "pareto-fountain-pen/interface/generated/liana/schemas/prose/data"
+import type * as d_in from "../../../../interface/generated/liana/schemas/authoring_target/data.js"
+
+namespace d_function {
+
+    export type Parameters = {
+        'write delimiters': boolean
+    }
+
+}
+
+export namespace interface_ {
+    export type Document = p_i.Transformer<
+        d_in.Document, d_out.Paragraph
+    >
+
+    export type Value = p_i.Transformer_With_Parameter<
+        d_in.Value,
+        d_out.Phrase.composed,
+        d_function.Parameters
+    >
+
+    export type Token_Trivia = p_i.Transformer<
+        d_in.Token_Trivia, d_out.Phrase.composed
+    >
+}
--- typescript/lib/src/interface/declarations/transformers/authoring_target/text.ts
+++ typescript/lib/src/interface/declarations/transformers/authoring_target/text.ts
@@ -1,1 +1,36 @@
-import type * as p_ from 'pareto-core/interface/transformer'
+import type * as p_i from 'pareto-core/interface/transformer'
+
+import type * as d_in from "../../../../interface/generated/liana/schemas/authoring_target/data.js"
+import type * as d_out from "pareto-fountain-pen/interface/generated/liana/schemas/text/data"
+
+//dependencies
+import * as t_to_prose from "./prose.js"
+import * as t_fountain_pen_to_text from "pareto-fountain-pen/implementation/manual/transformers/prose/text"
+
+//shorthands
+import * as sh from "pareto-fountain-pen/shorthands/prose/deprecated"
+
+export namespace interface_ {
+    export type Parameters = {
+        'write delimiters': boolean
+        'indentation': string,
+        'newline': string
+
+    }
+
+    export type Doc_Parameters = {
+        'indentation': string,
+        'newline': string
+    }
+
+    export type Value = p_i.Transformer_With_Parameter<
+        d_in.Value,
+        d_out.Text,
+        Parameters
+    >
+    export type Document = p_i.Transformer_With_Parameter<
+        d_in.Document,
+        d_out.Text,
+        Doc_Parameters
+    >
+}
