6b608ed03c83bf38afc7afeed39a377e5f55856e
Corno Schraverus <corno@schraverus.com>
2026-07-08 21:30:08 +0200

    pdt: ...

--- typescript/lib/src/interface/declarations/transformers/authoring_target/text.ts
+++ typescript/lib/src/interface/declarations/transformers/authoring_target/text.ts
@@ -3,14 +3,6 @@ import type * as p_ from 'pareto-core/interface/transformer'
 import type * as d_in from "../../../generated/liana/schemas/authoring_target/data.js"
 import type * as d_out from "pareto-fountain-pen/interface/generated/liana/schemas/text/data"
 
-//dependencies
-import * as t_to_prose from "./prose.js"
-import * as t_fountain_pen_to_text from "pareto-fountain-pen/implementation/manual/transformers/prose/text"
-
-//shorthands
-import * as sh from "pareto-fountain-pen/shorthands/prose/deprecated"
-
-
 export type Parameters = {
     'write delimiters': boolean
     'indentation': string,
