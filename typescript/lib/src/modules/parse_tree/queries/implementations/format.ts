3c768bccc5ab0ba5cab30521bd2e4323da8d1bc4
Corno Schraverus <corno@schraverus.com>
2026-08-02 14:36:34 +0200

    pdt: ...

--- typescript/lib/src/modules/parse_tree/queries/implementations/format.ts
+++ typescript/lib/src/modules/parse_tree/queries/implementations/format.ts
@@ -12,7 +12,7 @@ import * as r_parse_tree_from_list_of_characters from "astn-core/modules/deseria
 import * as ser_location from "astn-core/modules/deserialization/schemas/location/serializers"
 import * as ser_parse_tree_deserialization from "astn-core/modules/deserialization/schemas/parse_tree_deserialization/serializers"
 import * as ser_path from "pareto-filesystem-unrestricted-api/modules/unrestricted/schemas/path/serializers"
-import * as t_authoring_target_to_prose from "../../../authoring_target/schemas/authoring_target/transformers/paragraph.js"
+import * as t_authoring_target_to_paragraph from "../../../authoring_target/schemas/authoring_target/transformers/paragraph.js"
 import * as t_parse_tree_2_authoring_target from "../../schemas/parse_tree/transformers/authoring_target.js"
 import * as t_parse_tree_deserialization_to_location from "astn-core/modules/deserialization/schemas/parse_tree_deserialization/transformers/location"
 
@@ -57,7 +57,7 @@ export const $$: p_.Query_Implementation<
         )
     )).transform(
         ($) => ({
-            'paragraph': t_authoring_target_to_prose.Document(
+            'paragraph': t_authoring_target_to_paragraph.Document(
                 t_parse_tree_2_authoring_target.Document(
                     $
                 ),
